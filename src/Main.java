c09e4eb7a8c7f2fe36c89d3382ca082720a51ba0
ptcvas18 <VHil@luxoft.com>
2019-07-11 13:23:35 +0300

    Revert "Merge"
    This reverts commit af0703bdae066b92962655d75787b67796ab5eb2, reversing
    changes made to 74a812a53df0781f6c3f39af2b948911f9d173d2.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("WR!");
+        System.out.println("C1!");
         System.out.println("C2!");
-
         System.out.println("C3!");
         System.out.println("WR!");
         System.out.println("WR!");
